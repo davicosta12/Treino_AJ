c5f0b210ebe5de975b3299fb914fcef5bc476b2e
Davi <deadspace24@hotmail.com>
2021-02-04 13:18:41 -0300

    Alterações no Menu Exercício 03 React

--- React_exercicio/exercicio03/src/components/App/Menu/Menu.jsx
+++ React_exercicio/exercicio03/src/components/App/Menu/Menu.jsx
@@ -2,29 +2,65 @@ import './Menu.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const AppMenu = props => ( 
-  <header className="Menu">
-    <nav>
-      <ul>
-        <div className="leftSide">
-          <li>
-            <Link to="/usuarios">Usuários</Link>
-          </li>
-          <li>
-            <Link to="/vendedores">Vendedores</Link >
-          </li>
-        </div>
-        <div className="rightSide">
-          <li>
-            <p className="Menu-ul-li-p">Usuário: Davi</p>
-          </li>
-          <li>
-            <Link to="/login">Sair</Link>
-          </li>
-        </div>
-      </ul>
-    </nav>
-  </header>
-)
+import { makeStyles, withTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 
-export default AppMenu
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: withTheme,
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+            aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <Link className="link-menu" to="/usuarios"><MenuItem onClick={handleClose}>Usuários</MenuItem></Link>
+            <Link className="link-menu" to="/vendedores"><MenuItem onClick={handleClose}>Vendedores</MenuItem></Link>
+          </Menu>
+          <Typography variant="h6" className={classes.title}>
+            Menu
+          </Typography>
+          <Link className="link-btn-menu" to="/login">Sair</Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
